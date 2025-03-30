b438a7f1565a2f35dff8578befbe0b4b7007cbd3
Hexm3x <hexm3x@gmail.com>
2025-03-30 13:40:05 +0000

    Update train.sh

--- scripts/train.sh
+++ scripts/train.sh
@@ -39,12 +39,6 @@ Results_model_epochs_40_logint_100_emsize_200_nhid_200_drop_05
 
 #NOTE - training with different hyperparameter settings (epochs, log intervals, emsize, nhid)
 # To enable the training with different hyperparameters, comment out the "<<[name]" and the "[name]"
-# To not get confused with the submitted files, I made an alternative folder for my experimentation
-<<alt_models
-alt_models=$base/alt_models
-
-mkdir -p $alt_models
-alt_models
 
 <<model_epochs_30_logint_75_emsize_150_nhid_150_drop_05
 (cd $tools/pytorch-examples/word_language_model &&
@@ -52,7 +46,7 @@ alt_models
         --epochs 30 \
         --log-interval 75 \
         --emsize 150 --nhid 150 --dropout 0.5 --tied \
-        --save $alt_models/model_epochs_30_logint_75_emsize_150_nhid_150_drop_05.pt
+        --save $models/model_epochs_30_logint_75_emsize_150_nhid_150_drop_05.pt
 )
 <<Results_model_epochs_30_logint_75_emsize_150_nhid_150_drop_05
 | epoch  30 |    75/  121 batches | lr 20.00 | ms/batch 328.10 | loss  4.06 | ppl    58.20
@@ -74,7 +68,7 @@ model_epochs_30_logint_75_emsize_150_nhid_150_drop_05
         --epochs 25 \
         --log-interval 75 \
         --emsize 100 --nhid 100 --dropout 0.5 --tied \
-        --save $alt_models/model_epochs_25_logint_75_emsize_100_nhid_100_drop_05.pt
+        --save $models/model_epochs_25_logint_75_emsize_100_nhid_100_drop_05.pt
 )
 <<Results_model_epochs_25_logint_75_emsize_100_nhid_100_drop_05
 | epoch  25 |    75/  121 batches | lr 5.00 | ms/batch 368.78 | loss  4.27 | ppl    71.69
@@ -96,7 +90,7 @@ model_epochs_25_logint_75_emsize_100_nhid_100_drop_05
         --epochs 20 \
         --log-interval 50 \
         --emsize 100 --nhid 100 --dropout 0.5 --tied \
-        --save $alt_models/model_epochs_20_logint_50_emsize_100_nhid_100_drop_05.pt
+        --save $models/model_epochs_20_logint_50_emsize_100_nhid_100_drop_05.pt
 )
 <<Results_model_epochs_20_logint_50_emsize_100_nhid_100_drop_05
 | epoch  20 |    50/  121 batches | lr 20.00 | ms/batch 311.78 | loss  4.48 | ppl    87.80
@@ -119,7 +113,7 @@ model_epochs_20_logint_50_emsize_100_nhid_100_drop_05
         --epochs 5 \
         --log-interval 61 \
         --emsize 20 --nhid 20 --dropout 0.5 --tied \
-        --save $alt_models/model_epochs_5_logint_61_emsize_20_nhid_20_drop_05.pt
+        --save $models/model_epochs_5_logint_61_emsize_20_nhid_20_drop_05.pt
 )
 <<Results_model_epochs_5_logint_61_emsize_20_nhid_20_drop_05
 | epoch   5 |    61/  121 batches | lr 20.00 | ms/batch 135.62 | loss  5.65 | ppl   284.18
